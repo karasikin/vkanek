8c540355ac7ad47f6dda2ef27915debdef6dc1f9
karasikin <roma.mango.25@gmail.com>
2021-11-19 10:26:40 +0300

    Add sidebar to vk.

--- script.js
+++ script.js
@@ -3,12 +3,11 @@
 main();
 
 async function main() {
-    console.log('Extension [Vkanek] executed...');
-
-    let content;
+    console.log("Extension [Vkanek] executed...");
 
     try {
-        content = await loadContent("https://anekdot.ru/random/anekdot");
+        let content = await loadContent("https://anekdot.ru/random/anekdot");
+        addContentToPage(parseContent(content, "div.text"));
     } catch(e) {
         console.log(e);
     }
@@ -31,7 +30,32 @@ function parseContent(content, selector) {
     let elements = element.querySelectorAll(selector);
 
     return Array.from(elements).map(x => x.textContent);
-} 
+}
+
+function addContentToPage(content) {
+    let sidebar = createSidebar();
+
+    for(let text of content) {
+        let div = document.createElement('div');
+
+        div.innerHTML = text;
+        sidebar.appendChild(div);
+    }
+
+    document.body.appendChild(sidebar);
+}
+
+function createSidebar() {
+    let sidebar = document.createElement("div");
+
+    sidebar.style.position = "absolute";
+    sidebar.style.width = "200px";
+    sidebar.style.left = "10px";
+    sidebar.style.top = "40px";
+    /* sidebar settings... */
+
+    return sidebar;
+}
 
 class HttpStatusError extends Error {
     constructor(status) {
